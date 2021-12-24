de8b3a5a6324f27fd14ede260a3a7ea740a0a0b9
Mickdevv <46746774+Mickdevv@users.noreply.github.com>
2021-12-24 15:30:00 +0100

    Switch case demo class

--- ECommerceOrigine/ECommerceOrigine/src/Pages_Objets/DemoClass.java
+++ ECommerceOrigine/ECommerceOrigine/src/Pages_Objets/DemoClass.java
@@ -4,8 +4,41 @@ public class DemoClass {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int result = show(1, 2);
-		showInt(result);
+		//int result = show(1, 2);
+		//showInt(result);
+		
+		String x = "Tuesday";
+		switch(x) {
+		
+		case("Monday"):
+			System.out.println("It's Monday");
+			break;
+			
+		case("Tuesday"):
+			System.out.println("It's Tuesday");
+			break;
+			
+		case("Wednesday"):
+			System.out.println("It's Wednesday");
+			break;
+		}
+		
+		
+		
+		if(x == "Monday")
+		{
+			System.out.println("It's Monday");
+		}
+		else if(x == "Tuesday")
+		{
+			System.out.println("It's Tuesday");
+		}
+		else if(x == "Wednesday")
+		{
+			System.out.println("It's Wednesday");
+		}
+		
+		
 		
 	}
 	
